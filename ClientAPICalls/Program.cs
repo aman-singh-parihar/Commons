1ab9d9e1d628d6ae70743d4422bf8732d6e0cefb
Aman Singh Parihar <amanparihar16@gmail.com>
2023-06-07 10:29:23 +0530

    Update Program.cs

--- ClientAPICalls/Program.cs
+++ ClientAPICalls/Program.cs
@@ -4,27 +4,6 @@
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Some Method Started");
-            using (var client = new HttpClient())
-            {
-                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(4000);
-                client.BaseAddress = new Uri("http://localhost:58937/");
-                try
-                {
-                    string Name = "James";
-                    Console.WriteLine("Some Method Calling Web API");
-                    HttpResponseMessage response = await client.GetAsync($"api/greetings/{Name}", cancellationTokenSource.Token);
-                    string message = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(message);
-                }
-                catch (TaskCanceledException ex)
-                {
-                    Console.WriteLine($"Task Execution Cancelled: {ex.Message}");
-                }
-                Console.WriteLine("Some Method Completed");
-            }
-
-            Console.WriteLine("Hello, World!");
             CancellationTokenSource cts = new CancellationTokenSource();
             cts.CancelAfter(TimeSpan.FromSeconds(5));
             await LongRunningTask(100, cts.Token);
