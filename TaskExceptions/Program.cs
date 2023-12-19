409c4b907e2c6c0942e9c41c0fad40d697cbfb9c
Aman Singh Parihar <amanparihar16@gmail.com>
2023-12-19 13:50:36 +0000

    task

---
+++ TaskExceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿var tasks = new List<Task>();
+Func<Task> function1 = async () =>
+{
+    await Task.Delay(2000);
+    Console.WriteLine("After some time");
+    throw new Exception(message: "Task Exception1");
+};
+Func<Task> function2 = async () =>
+{
+    await Task.Delay(2000);
+    Console.WriteLine("After some time");
+    throw new Exception(message: "Task Exception2");
+};
+dynamic oneTask = await Task.Factory.StartNew(function1);
+oneTask = "3";
+var TwoTask = await Task.Factory.StartNew(function2);
+
+tasks.Add(oneTask);
+tasks.Add(TwoTask);
+try
+{
+    await Task.WhenAll(tasks);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
