fec4f3e415d3e35f9c5b8ab60164a549227b224c
Aman Singh Parihar <amanparihar16@gmail.com>
2024-01-24 13:14:28 +0000

    Delegates and events

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,28 @@
+﻿
+DelEventHandler d = USA;
+d += India;
+d += England;
+//add += new DelEventHandler(USA);
+//add += new DelEventHandler(India);
+//add += new DelEventHandler(England);
+add += d;
+add.Invoke();
+Console.ReadLine();
+public static event DelEventHandler add;
+public delegate void DelEventHandler();
+
+static void USA()
+{
+    Console.WriteLine("USA");
+}
+
+static void India()
+{
+    Console.WriteLine("India");
+}
+
+static void England()
+{
+    Console.WriteLine("England");
+}
+
